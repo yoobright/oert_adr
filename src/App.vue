b70887771859a0a036e7540d507606a1bc6c3b73
yoobright <yxj3179@gmail.com>
2023-03-24 16:05:18 +0800

    add locale icon

--- src/App.vue
+++ src/App.vue
@@ -43,7 +43,14 @@ const changeLang = (lang: string) => {
     </div>
     <div class="vertical-header-right">
       <el-form :inline="true">
-        <el-form-item label="Lang" style="color: white !important">
+        <el-form-item label="" style="color: white !important">
+          <div style="color: #E5EAF3; margin-right: 10px; display: flex; align-items: center;">
+            <svg preserveAspectRatio="xMidYMid meet"  viewBox="0 0 24 24" height="24" width="24">
+            <path fill="currentColor"
+              d="m18.5 10l4.4 11h-2.155l-1.201-3h-4.09l-1.199 3h-2.154L16.5 10h2zM10 2v2h6v2h-1.968a18.222 18.222 0 0 1-3.62 6.301a14.864 14.864 0 0 0 2.336 1.707l-.751 1.878A17.015 17.015 0 0 1 9 13.725a16.676 16.676 0 0 1-6.201 3.548l-.536-1.929a14.7 14.7 0 0 0 5.327-3.042A18.078 18.078 0 0 1 4.767 8h2.24A16.032 16.032 0 0 0 9 10.877a16.165 16.165 0 0 0 2.91-4.876L2 6V4h6V2h2zm7.5 10.885L16.253 16h2.492L17.5 12.885z">
+            </path>
+          </svg>
+          </div>
           <el-select v-model="region" @change="changeLang(region)" style="width: 64px">
             <el-option value="zh">中文</el-option>
             <el-option value="en">English</el-option>
@@ -99,6 +106,10 @@ const changeLang = (lang: string) => {
   min-width: 280px;
   margin-left: auto;
 }
+
+/* .navbar .el-form-item__label {
+  color: white !important;
+} */
 
 @media only screen and (max-width: 480px) {
   .navbar {
